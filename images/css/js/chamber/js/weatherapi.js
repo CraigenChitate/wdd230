49092df2fa11e85120ea8461b32071a0fb6258e4
CraigenChitate <chi22013@byui.edu>
2022-10-07 04:41:05 +0200

    uploaaded my site with new pictures

---
+++ images/css/js/chamber/js/weatherapi.js
@@ -0,0 +1,55 @@
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#currentTemp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#weather-desc');
+const Speed = document.querySelector('#windSpeed');
+
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Argentina,AR&appid=f2f2ad01b4c35a18e025d561569710c4&units=imperial";
+
+
+async function apiFetch(apiURL) {
+    
+    const response = await fetch(apiURL);
+    if (response.ok) {
+      const data = await response.json();
+
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } 
+  apiFetch(url);
+
+function displayResults(weatherData){
+
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.innerHTML = desc;
+    Speed.innerHTML =  weatherData.wind.speed;
+
+    const temp = weatherData.main.temp.toFixed(0);
+    const windChill = document.getElementById("windChill");
+    const windS = weatherData.wind.speed;
+    
+    console.log(temp)
+    console.log(windS)
+    
+    if (temp <= 50 && windS > 3) {
+    
+        const windChill_f = 35.74 + (0.6215 * temp) - (35.75 * windS**0.16) + (0.4275 * temp * windS**0.16);
+        windChill.innerText = Math.round(windChill_f);
+    
+    }
+    
+    else {
+        windChill.innerText = "N/A";
+    }
+}
+
