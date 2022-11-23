6ed75ca501c101a8a155ccadaf71fcd2443a470b
CraigenChitate <chi22013@byui.edu>
2022-11-23 05:42:51 +0200

    added contact page html file

--- images/css/js/chamber/js/weatherapi.js
+++ images/css/js/chamber/js/weatherapi.js
@@ -1,4 +1,3 @@
-
 // select HTML elements in the document
 const currentTemp = document.querySelector('#currentTemp');
 const weatherIcon = document.querySelector('#weather-icon');
@@ -6,25 +5,24 @@ const captionDesc = document.querySelector('#weather-desc');
 const Speed = document.querySelector('#windSpeed');
 
 
-<<<<<<< HEAD
-const url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c39b8cb0c4372beb97c3c6f080a86d40";
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Johannesburg,ZA&APPID=c39b8cb0c4372beb97c3c6f080a86d40";
 
 
 async function apiFetch(apiURL) {
-    
+
     const response = await fetch(apiURL);
     if (response.ok) {
-      const data = await response.json();
+        const data = await response.json();
 
-      console.log(data); // this is for testing the call
-      displayResults(data);
+        console.log(data); // this is for testing the call
+        displayResults(data);
     } else {
         throw Error(await response.text());
     }
-  } 
-  apiFetch(url);
+}
+apiFetch(url);
 
-function displayResults(weatherData){
+function displayResults(weatherData) {
 
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
 
@@ -33,24 +31,21 @@ function displayResults(weatherData){
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.innerHTML = desc;
-    Speed.innerHTML =  weatherData.wind.speed;
+    Speed.innerHTML = weatherData.wind.speed;
 
     const temp = weatherData.main.temp.toFixed(0);
     const windChill = document.getElementById("windChill");
     const windS = weatherData.wind.speed;
-    
+
     console.log(temp)
     console.log(windS)
-    
+
     if (temp <= 50 && windS > 3) {
-    
-        const windChill_f = 35.74 + (0.6215 * temp) - (35.75 * windS**0.16) + (0.4275 * temp * windS**0.16);
+
+        const windChill_f = 35.74 + (0.6215 * temp) - (35.75 * windS ** 0.16) + (0.4275 * temp * windS ** 0.16);
         windChill.innerText = Math.round(windChill_f);
-    
-    }
-    
-    else {
+
+    } else {
         windChill.innerText = "N/A";
     }
 }
-
